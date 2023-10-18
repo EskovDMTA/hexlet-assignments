dd5aa51780f59e71c791176bb4ccb1bd2fa735ae
@hexlet/cli <support@hexlet.io>
2023-10-18 22:16:48 +0300

    submit rails-basics-ru/crud

--- rails-basics-ru/crud/test/controllers/test_tasks_controller.rb
+++ rails-basics-ru/crud/test/controllers/test_tasks_controller.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 require_relative '../test_helper'
 require_relative '../../app/controllers/tasks_controller'
 class TestTasksController < ActionDispatch::IntegrationTest
@@ -10,7 +11,7 @@ class TestTasksController < ActionDispatch::IntegrationTest
       status: 0.to_s,
       creator: Faker::Name.unique.name,
       performer: Faker::Name.unique.name,
-      completed: index.even?
+      completed: 0.even? ? true : false
     )
   end
 
@@ -21,7 +22,7 @@ class TestTasksController < ActionDispatch::IntegrationTest
       status: 0.to_s,
       creator: Faker::Name.unique.name,
       performer: Faker::Name.unique.name,
-      completed: index.even?
+      completed: 0.even? ? true : false
     )
     assert_equal(@task.valid?, true)
   end
