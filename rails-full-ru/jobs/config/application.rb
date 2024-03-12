b16c60c9ec9bd1af3f9e768a7983b1286aff3bad
@hexlet/cli <support@hexlet.io>
2024-03-12 10:23:55 +0300

    submit rails-full-ru/jobs

--- rails-full-ru/jobs/config/application.rb
+++ rails-full-ru/jobs/config/application.rb
@@ -24,7 +24,7 @@ module Job
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
-    config.active_job.queue_adapter = :sidekiq
+    # config.active_job.queue_adapter = :sidekiq
 
     # Configuration for the application, engines, and railties goes here.
     #
